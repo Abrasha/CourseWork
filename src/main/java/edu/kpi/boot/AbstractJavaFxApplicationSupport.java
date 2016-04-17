b57985017f7ff5a15282ee75c8148dfdd3f53239
Abrasha <aabrasha@gmail.com>
2016-04-17 19:28:20 +0300

    + LoggerFactory + AutoClosing resources after application cycle

--- src/main/java/edu/kpi/boot/AbstractJavaFxApplicationSupport.java
+++ src/main/java/edu/kpi/boot/AbstractJavaFxApplicationSupport.java
@@ -1,5 +1,6 @@
 package edu.kpi.boot;
 
+import edu.kpi.settings.logger.factory.LoggerFactory;
 import javafx.application.Application;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,6 +11,11 @@ public abstract class AbstractJavaFxApplicationSupport extends Application {
 
     protected ConfigurableApplicationContext context;
 
+    protected static void launchApp(Class<? extends AbstractJavaFxApplicationSupport> appClass, String[] args) {
+        AbstractJavaFxApplicationSupport.savedArgs = args;
+        Application.launch(appClass, args);
+    }
+
     @Override
     public void init() throws Exception {
         context = SpringApplication.run(getClass(), savedArgs);
@@ -19,11 +25,7 @@ public abstract class AbstractJavaFxApplicationSupport extends Application {
     @Override
     public void stop() throws Exception {
         super.stop();
+        LoggerFactory.closeLoggers();
         context.close();
-    }
-
-    protected static void launchApp(Class<? extends AbstractJavaFxApplicationSupport> appClass, String[] args) {
-        AbstractJavaFxApplicationSupport.savedArgs = args;
-        Application.launch(appClass, args);
     }
 }
---
+++ src/main/java/edu/kpi/settings/logger/factory/LoggerFactory.java
@@ -0,0 +1,86 @@
+package edu.kpi.settings.logger.factory;
+
+import edu.kpi.settings.logger.ConsoleLogger;
+import edu.kpi.settings.logger.FileLogger;
+import edu.kpi.settings.logger.Logger;
+import edu.kpi.settings.logger.decorator.LeveledLogger;
+import edu.kpi.settings.logger.decorator.TimeStampLogger;
+
+import java.io.File;
+import java.io.IOException;
+
+// TODO Factory Pattern
+public class LoggerFactory {
+
+    public static final String LOG_FILE = "application.log";
+
+    public static Logger getLogger(LoggerType type, LoggerAppender... appenders) {
+
+        Logger result = parseType(type);
+        result = decorate(result, appenders);
+
+        return result;
+    }
+
+    private static Logger decorate(final Logger logger, LoggerAppender[] appenders) {
+
+        Logger result = logger;
+
+        for (LoggerAppender appender : appenders) {
+            switch (appender) {
+                case TIMESTAMP:
+                    result = new TimeStampLogger(result);
+                    break;
+                case LEVEL:
+                    result = new LeveledLogger(result);
+                    break;
+                default:
+                    throw new IllegalArgumentException("No such Logger appender: " + appender);
+            }
+        }
+
+        return result;
+    }
+
+    private static Logger parseType(LoggerType type) {
+        switch (type) {
+            case CONSOLE:
+                return LazyConsoleLoggerKeeper.INSTANCE;
+            case FILE:
+                return LazyFileLoggerKeeper.INSTANCE;
+            default:
+                throw new IllegalArgumentException("No such Logger type: " + type);
+        }
+    }
+
+    public static void closeLoggers() {
+        LazyFileLoggerKeeper.INSTANCE.getPrintStream().close();
+    }
+
+    public enum LoggerType {
+        FILE, CONSOLE
+    }
+
+    public enum LoggerAppender {
+        TIMESTAMP, LEVEL
+    }
+
+    private static class LazyFileLoggerKeeper {
+        private static final Logger INSTANCE;
+
+        static {
+            Logger temp;
+            try {
+                temp = new FileLogger(new File(LOG_FILE));
+            } catch (IOException e) {
+                temp = null;
+            }
+            INSTANCE = temp;
+        }
+    }
+
+    private static class LazyConsoleLoggerKeeper {
+        private static final Logger INSTANCE = new ConsoleLogger();
+    }
+
+}
