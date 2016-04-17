621891d8f8b7f0e58ab31f5506d4f6b4ff755c43
Abrasha <aabrasha@gmail.com>
2016-04-17 17:10:46 +0300

    + JavaFX Spring Boot Starter & App Launcher

---
+++ src/main/java/edu/kpi/boot/Application.java
@@ -0,0 +1,36 @@
+package edu.kpi.boot;
+
+import edu.kpi.settings.ConfigurationControllers;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Lazy;
+
+@Lazy
+@SpringBootApplication
+public class Application extends AbstractJavaFxApplicationSupport {
+
+    @Value("${ui.title}")//
+    private String windowTitle;
+
+    @Qualifier("employeesView")
+    @Autowired
+    private ConfigurationControllers.View view;
+
+    public static void main(String[] args) {
+        launchApp(Application.class, args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle(windowTitle);
+        stage.setScene(new Scene(view.getView()));
+        stage.setResizable(true);
+        stage.centerOnScreen();
+        stage.show();
+    }
+
+}
